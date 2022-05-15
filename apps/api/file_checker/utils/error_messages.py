21b8132a2149f1bdd93a3c17e4f6c1f21b015e4e
ghettoDdOS <pen.egor2002@gmail.com>
2022-05-15 11:52:12 +0300

    fix msg

--- apps/api/file_checker/utils/error_messages.py
+++ apps/api/file_checker/utils/error_messages.py
@@ -2,5 +2,5 @@ class ErrorMessages:
     COMBINED_FILE = "Файл является вредоносным"
     CORRUPTED_FILE = "Файл не безопасный"
     HAS_MACRO_BUT_OK = "Фалй содержит макросы"
-    HAS_VIRUS_MACRO = "Файл содержить подозрительные макросы"
+    HAS_VIRUS_MACRO = "Файл содержит подозрительные макросы"
     HAS_JS = "Файл содержит подозрительные скрипты"
