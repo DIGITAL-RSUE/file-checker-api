9d019ba96efe2bb9b378160772b89dec05b87a61
Dmitriy <58954855+lenston19@users.noreply.github.com>
2022-05-15 11:59:41 +0300

    Update error_messages.py

--- apps/api/file_checker/utils/error_messages.py
+++ apps/api/file_checker/utils/error_messages.py
@@ -1,6 +1,6 @@
 class ErrorMessages:
     COMBINED_FILE = "Файл является вредоносным"
     CORRUPTED_FILE = "Файл не безопасный"
-    HAS_MACRO_BUT_OK = "Фалй содержит макросы"
+    HAS_MACRO_BUT_OK = "Файл содержит макросы"
     HAS_VIRUS_MACRO = "Файл содержит подозрительные макросы"
     HAS_JS = "Файл содержит подозрительные скрипты"
